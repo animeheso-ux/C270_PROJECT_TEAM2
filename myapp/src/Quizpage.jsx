dba5168335f18becd7043646cdc0bbd7c4f97a3d
animeheso-ux <animeheso@gmail.com>
2026-07-02 22:21:36 +0800

    added verifytoken function to quiz page

--- myapp/src/Quizpage.jsx
+++ myapp/src/Quizpage.jsx
@@ -1,7 +1,45 @@
+import { useEffect } from "react"
+
+
+
 import "./QuizPage.css"
 
 
 function QuizPage({ToLogin}) {
+
+    async function VerifyToken() {
+        const Token = localStorage.getItem("Token")
+
+        if (!Token) {
+            ToLogin()
+            return
+        }
+
+        const response = await fetch("/GetToken",{
+            headers : {
+                "authorization" : `Bearer ${Token}`,
+                "Content-Type" : "application/json"
+            }
+            
+        })
+
+        const data = await response.json()
+
+
+        if (data.status != "success") {
+            console.log("Invalid!")
+            ToLogin()
+        }
+
+        
+    }
+
+
+
+    useEffect(()=> {
+        if (location.port != 3000) {return}
+        VerifyToken()
+    },[])
 
 
 
