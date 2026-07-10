e964350f2a2a49570721311cab5a0210149fb6c3
Sherri <23044568@myrp.edu.sg>
2026-07-10 23:40:26 +0800

    feature: reorganise folders, added docker-compose.yml and dockerfiles

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const { exec } = require("node:child_process")
+
+const http = require("http")
+const path = require("path")
+
+const { DatabaseRouter, database } = require("./database")
+const { QuizRouter } = require("./Quiz")
+const { TokenRouter } = require("./token")
+
+const app = express()
+const PORT = 3000
+
+const server = http.createServer(app)
+
+app.use(express.json())
+//NOTE: PLEASE DON'T ADD ANY ROUTES HERE THIS IS FOR SERVER OPENING
+//routes
+
+app.use(DatabaseRouter)
+app.use(QuizRouter)
+app.use(TokenRouter)
+
+app.use(express.static(path.join(__dirname, "myapp/dist")))
+
+
+app.get("/{*path}", (req, res) => {
+    res.sendFile(path.join(__dirname, "myapp/dist", "index.html"))
+})
+
+server.listen(PORT, () => console.log("localhost connected"), exec(`start http://localhost:${PORT}`))
